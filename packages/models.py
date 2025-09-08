aa00696e1a7afe6ae620d5c0010dc096d0d6e307
Mark Hallett <markhallett5@outlook.com>
2025-09-08 23:16:12 +0100

    removed feature field and migrated

--- packages/models.py
+++ packages/models.py
@@ -8,7 +8,6 @@ class Package(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    features = models.TextField(blank=True)
     image = models.ImageField(upload_to='packages/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)

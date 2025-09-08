78a14fbf6ef7368e10651d110b9ae8ea7add0bb5
Mark Hallett <markhallett5@outlook.com>
2025-09-08 22:41:47 +0100

    Create packages app and update models.py

---
+++ webworks/packages/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+
+
+class Package(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    features = models.TextField(blank=True)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created_at']),
+        ]
+        verbose_name = "Package"
+        verbose_name_plural = "Packages"
+
+    def __str__(self):
+        return self.name
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)

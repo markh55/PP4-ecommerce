95c7eb6309c5271a3ac503a5d8bbac05fb354572
Mark Hallett <markhallett5@outlook.com>
2025-09-08 22:48:19 +0100

    updated packages/admin.py

--- webworks/packages/admin.py
+++ webworks/packages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Package
 
-# Register your models here.
+@admin.register(Package)
+class PackageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'available', 'created_at', 'updated_at')
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ('available', 'created_at', 'updated_at')
+    search_fields = ('name', 'description')

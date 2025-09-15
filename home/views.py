b5f691b14eb8e904dca3da089823d2f81ca19206
Mark Hallett <markhallett5@outlook.com>
2025-09-15 22:56:56 +0100

    setup contact form and styled in form.css

--- home/views.py
+++ home/views.py
@@ -1,11 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 from packages.models import Package
+from .forms import ContactForm
 
-# Create your views here.
+# Home page
 def index(request):
     packages = Package.objects.all()[:4]
-    return render(request, 'home/index.html', {'packages': packages})
 
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            send_mail(
+                subject=f"New Inquiry: {form.cleaned_data['inquiry_type']}",
+                message=(
+                    f"Name: {form.cleaned_data['name']}\n"
+                    f"Email: {form.cleaned_data['email']}\n\n"
+                    f"Message:\n{form.cleaned_data['message']}"
+                ),
+                from_email=form.cleaned_data["email"],
+                recipient_list=["youremail@example.com"],
+            )
+            return redirect("index")
+    else:
+        form = ContactForm()
+
+    return render(request, 'home/index.html', {
+        'packages': packages,
+        'form': form,
+    })
+
+# Packages page
 def packages(request):
     packages = Package.objects.all()
     return render(request, 'home/packages.html', {'packages': packages})

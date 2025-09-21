4eb3fe5ae95cd74d5cf613aa03af699493638630
Mark Hallett <markhallett5@outlook.com>
2025-09-21 23:09:48 +0100

    Added checkout app, implemented add/remove from cart, styled checkout, and added cart item count indicator in navbar

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from packages.models import Package
 
 def bag_contents(request):
     context = {}
@@ -6,6 +8,17 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Package, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     grand_total = total
 
--- bag/urls.py
+++ bag/urls.py
@@ -6,4 +6,5 @@ app_name = 'bag'
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<int:items_id>/', views.add_to_bag, name='add_to_bag'),
+    path('remove/<int:items_id>/', views.remove_from_bag, name='remove_from_bag'),
 ]

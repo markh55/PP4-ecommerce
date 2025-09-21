887033f35c65b72a1fbe29bab8f78f979416f39f
Mark Hallett <markhallett5@outlook.com>
2025-09-21 17:55:10 +0100

    Set up Add to Bag feature and redirected 'Buy Now' button to add to basket

---
+++ bag/contexts.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+
+def bag_contents(request):
+    context = {}
+
+    bag_items = []
+    total = 0
+    product_count = 0
+
+    grand_total = total
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'grand_total': grand_total,
+    }
+
+    return context
+
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,17 @@ def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
+def add_to_bag(request, items_id):
+    quantity = 1
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if items_id in list(bag.keys()):
+        bag[items_id] += quantity
+    else:
+        bag[items_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)

488a8061c5d650325624fca6f6b443dda8c84861
Mark Hallett <markhallett5@outlook.com>
2025-09-28 13:57:34 +0100

    Updated checkout template to suit webworks theme

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from packages.models import Package
 
-# Create your views here.
 @login_required
 def checkout(request):
-    return render(request, 'checkout/checkout.html')
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products'))
+
+    services_summary = []
+    total = 0
+    for package_id in bag.keys():
+        package = Package.objects.get(pk=package_id)
+        services_summary.append({
+            'id': package.id,
+            'name': package.name,
+            'image': package.image,
+            'price': package.price
+        })
+        total += float(package.price)
+
+    context = {
+        'services_summary': services_summary,
+        'total': total,
+        'grand_total': total,
+    }
+
+    return render(request, 'checkout/checkout.html', context)

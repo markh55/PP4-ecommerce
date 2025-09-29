5c8f24aa1c5c7f0e1ca4785468d656fbd99e772d
Mark Hallett <markhallett5@outlook.com>
2025-09-29 22:03:51 +0100

    Added toast notifications and shopping bag preview

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from packages.models import Package
 
 # Create your views here.
 
@@ -8,14 +10,17 @@ def view_bag(request):
 
 
 def add_to_bag(request, items_id):
+    package = Package.objects.get(id=items_id)
     quantity = 1
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if items_id in list(bag.keys()):
         bag[items_id] += quantity
+        messages.success(request, f'Updated {package.name} quantity in your bag')
     else:
         bag[items_id] = quantity
+        messages.success(request, f'Added {package.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -29,5 +34,6 @@ def remove_from_bag(request, items_id):
     if items_id in bag:
         del bag[items_id]
         request.session['bag'] = bag
+        messages.success(request, 'Item removed from your bag')
 
     return redirect('bag:view_bag')
--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,11 @@
+// Show toast notifications
+document.addEventListener('DOMContentLoaded', function() {
+    const toasts = document.querySelectorAll('.toast');
+    toasts.forEach(toast => {
+        $(toast).toast('show');
+    });
+});
+
 //success and error messages fade out after 4 seconds//
 document.addEventListener('DOMContentLoaded', function() {
     setTimeout(function() {

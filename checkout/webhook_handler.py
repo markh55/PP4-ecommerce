5a26044932f95f2a82130896a19e834fe10a9d56
Mark Hallett <markhallett5@outlook.com>
2025-10-07 23:29:39 +0100

    Update Stripe webhook to handle save_info and mark orders as paid

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-
+from checkout.models import Order
+import stripe
 
 class StripeWH_Handler:
     """Handle Stripe webhooks."""
@@ -13,17 +14,37 @@ class StripeWH_Handler:
         """
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
-            status=200)
+            status=200
+        )
 
     def handle_payment_intent_succeeded(self, event):
         """
         Handle the payment_intent.succeeded webhook from Stripe.
         """
         intent = event.data.object
-        print(intent)
+        pid = intent.id
+        # save info / order history
+        save_info = intent.metadata.get('save_info')
+        order_number = intent.metadata.get('order_number')
+
+        if order_number:
+            try:
+                order = Order.objects.get(order_number=order_number)
+                order.payment_status = 'Paid'
+                order.save()
+                if save_info == 'true' and order.user:
+                    profile = order.user.profile
+                    stripe_charge = stripe.Charge.retrieve(intent.latest_charge)
+                    profile.default_phone_number = stripe_charge.billing_details.phone
+                    profile.default_email = stripe_charge.billing_details.email
+                    profile.save()
+            except Order.DoesNotExist:
+                pass
+
         return HttpResponse(
             content='Payment intent succeeded!',
-            status=200)
+            status=200
+        )
 
     def handle_payment_intent_payment_failed(self, event):
         """
@@ -31,4 +52,5 @@ class StripeWH_Handler:
         """
         return HttpResponse(
             content='Payment intent failed.',
-            status=200)
+            status=200
+        )

0f3a95780fbdc268e49e8297e997c6f59f3f297b
Mark Hallett <markhallett5@outlook.com>
2025-10-11 19:12:57 +0100

    Fix profile view and URLs: set view to `profile`, add app_name for namespace, and change route to /profile/

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-def profile_view(request):
+def profile(request):
     template = 'profiles/profile.html'
     context = {}
     return render(request, template, context)

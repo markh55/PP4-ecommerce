2ef023af08a03e3905cbb334a15ace7a6b67fd5a
Mark Hallett <markhallett5@outlook.com>
2025-10-17 21:38:03 +0100

    Added order details view to display order data

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
 from .forms import UserProfileForm
+import ast
 
 # Create your views here.
 def profile(request):
@@ -27,9 +28,16 @@ def order_history(request, order_number):
     profile = get_object_or_404(UserProfile, user=request.user)
     order = get_object_or_404(profile.orders, order_number=order_number)
 
+    # import order items from original bag
+    try:
+        order_items = ast.literal_eval(order.original_bag)
+    except:
+        order_items = []
+
     template = 'orders/order_details.html'
     context = {
         'order': order,
+        'order_items': order_items,
         'from_profile': True,
     }
 

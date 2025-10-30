d614897b232da79a7a4710a86eb5bd17b3e3eb73
Mark Hallett <markhallett5@outlook.com>
2025-10-30 13:02:18 +0000

    Update frontend: migrate Bootstrap 4 → 5 and adjust classes pt1

--- webworks/settings.py
+++ webworks/settings.py
@@ -43,7 +43,12 @@ INSTALLED_APPS = [
     'checkout',
     'profiles',
     'storages',
+    'crispy_forms',
+    "crispy_bootstrap5",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

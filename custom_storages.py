eab6d8f96c5b20219f8b9cd5f1827770e46ae6c6
Mark Hallett <markhallett5@outlook.com>
2025-11-02 02:00:53 +0000

    set up settings.py to show webp files on heroku

--- custom_storages.py
+++ custom_storages.py
@@ -1,5 +1,8 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
+import mimetypes
+
+mimetypes.add_type("image/webp", ".webp", True)
 
 
 class StaticStorage(S3Boto3Storage):
@@ -8,4 +11,3 @@ class StaticStorage(S3Boto3Storage):
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
-
--- webworks/settings.py
+++ webworks/settings.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 import dj_database_url
+import mimetypes
 from pathlib import Path
 from dotenv import load_dotenv
+
+mimetypes.add_type("image/webp", ".webp", True)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
